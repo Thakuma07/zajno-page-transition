9466eb1d64b28f2ae250528928f0ed272f688a5d
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2025-12-15 11:45:01 +0530

    Initial commit - Next.js Project

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { useRevealer } from "@/hooks/useRevealer";
+
+export default function Home() {
+  useRevealer();
+  
+  return (
+    <>
+      <div className ="revealer"></div>
+      <div className="home">
+        <div className="header">
+          <h1>Zayno</h1>
+        </div>
+      
+
+      <div className="hero-img">
+        <img src="/hero.jpg" alt=""/>  
+      </div> 
+      </div>
+    </>
+  );
+}
